cf2c6b36adb64a9575effadce23ba2e1dd70608c
Giordano Díaz <diazgiordano@gmail.com>
2020-08-13 16:15:49 -0500

    First commit creating bases

---
+++ ciclos.rb
@@ -0,0 +1,23 @@
+# Ciclos
+
+# x = 1
+
+# while x < 5 do
+#     puts "hello #{x}"
+#     x+=1
+# end
+
+# y = 1
+
+# loop do
+#     puts "Hello #{y}"
+#     break if y >= 5
+#     y +=1
+# end
+
+# for i in 1..4 do
+#     puts "Hello #{i}"
+# end
+
+# [1,2,3,4].each { |x| puts "Hello #{x}"}
+4.times { |x| puts "Hello #{x}" }
